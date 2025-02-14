aeab8117675d0a475cb4c7078ca0e19a089e9704
FilippieGit <filippieamaral@gmail.com>
2025-02-14 20:27:21 -0300

    v3

--- dbpadaria.sql
+++ dbpadaria.sql
@@ -22,8 +22,8 @@ dataNasc datetime,
 salario decimal(9,2) default 0 check (salario >= 0),
 sexo char(1) default "?" check(sexo in("F","M")),
 codGen int not null,
-primary key(codFunc)
-foreign key(codGen) references tbGenero(codFunc)
+primary key(codFunc),
+foreign key(codGen) references tbGenero(codGen)
 );
 
 create table tbUsuarios(
@@ -35,9 +35,31 @@ primary key(codUsu),
 foreign key(codFunc) references tbFuncionarios(codFunc)
 );
 
+create table tbFornecedores(
+codForn int not null auto_increment,
+nome varchar(100),
+email varchar(100),
+telCel char(10),
+primary key(codForn)
+);
+
+create table tbProdutos(
+codProd int not null auto_increment,
+descricao varchar(100),
+quantidade int,
+preco decimal(9,2),
+dataVal datetime,
+lote char(10),
+primary key(codProd),
+foreign key(codForn)references tbFornecedores(codForn)
+);
+
 -- visualizando as tabelas
 show tables;
 
 -- visualizando a estrutura das tabelas
 desc tbUsuarios;
 desc tbFuncionarios;
+desc tbGenero;
+desc tbFornecedores;
+desc tbProdutos;
