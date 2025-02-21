539c3e92f6003d89bed557795c63cf3956e04c83
FilippieGit <filippieamaral@gmail.com>
2025-02-21 20:13:38 -0300

    tabela clientes, -genero, inserindo dados

--- dbpadaria.sql
+++ dbpadaria.sql
@@ -6,11 +6,17 @@ create database dbpadaria;
 -- acessar o banco de dados
 use dbpadaria;
 
+/* Tabela gênero
 create table tbGenero(
 codGen int not null auto_increment,
 nome varchar(100),
 primary key(codGen)
 );
+
+--codGen int not null,
+
+--foreign key(codGen) references tbGenero(codGen)
+*/
 
 --criando as tabelas no banco de dados
 create table tbFuncionarios(
@@ -21,9 +27,7 @@ telCel char(10),
 dataNasc datetime,
 salario decimal(9,2) default 0 check (salario >= 0),
 sexo char(1) default "?" check(sexo in("F","M")),
-codGen int not null,
-primary key(codFunc),
-foreign key(codGen) references tbGenero(codGen)
+primary key(codFunc)
 );
 
 create table tbUsuarios(
@@ -35,6 +39,7 @@ primary key(codUsu),
 foreign key(codFunc) references tbFuncionarios(codFunc)
 );
 
+
 create table tbFornecedores(
 codForn int not null auto_increment,
 nome varchar(100),
@@ -42,6 +47,7 @@ email varchar(100),
 telCel char(10),
 primary key(codForn)
 );
+
 
 create table tbProdutos(
 codProd int not null auto_increment,
@@ -55,12 +61,48 @@ primary key(codProd),
 foreign key(codForn)references tbFornecedores(codForn)
 );
 
+create table tbClientes(
+codCli int not null auto_increment,
+nome varchar(100) not null,
+cpf char(14) not null unique,
+email varchar(100) unique,
+primary key(codCli)
+);
+
+create table tbVendas(
+codVend int not null auto_increment,
+dataVend date,
+horaVend time,
+quanVend decimal(9,2) default 0 check (quanVend >=0),
+valVend decimal(9,2) default 0 check (valVend >=0),
+codProd int not null,
+codUsu int not null,
+codCli int not null,
+primary key(codVend),
+foreign key(codCli)references tbClientes(codCli),
+foreign key(codUsu)references tbUsuarios(codUsu),
+foreign key(codProd) references tbProdutos(codProd)
+);
+
 -- visualizando as tabelas
 show tables;
 
 -- visualizando a estrutura das tabelas
 desc tbUsuarios;
 desc tbFuncionarios;
-desc tbGenero;
+--desc tbGenero;
 desc tbFornecedores;
 desc tbProdutos;
+
+--inserindo registros nas tabelas
+
+insert into tbClientes(nome,cpf,email) values('Davi Tures', '123.456.789-99', 'davitures@gmail.com');
+insert into tbClientes(nome,cpf,email) values('Filippie Zass', '214.144.144-99', 'filippiezass@gmail.com');
+insert into tbFornecedores(nome,email,telCel) values('Kappa Frutas','kappafrutas@tutamail.com','914916950');
+insert into tbFuncionarios(nome,email,telCel,dataNasc,salario,sexo) values('Rodrigo Silva','rodrigos@proton.com','927471128','1995-03-22',1500.05,'M');
+
+--visualizando os registros nas tabelas
+
+Select * from tbClientes;
+Select * from tbFornecedores;
+select * from tbFuncionarios;
